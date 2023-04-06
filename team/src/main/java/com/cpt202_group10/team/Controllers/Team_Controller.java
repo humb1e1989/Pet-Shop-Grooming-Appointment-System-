6b653e9772aa5785e35695af29a69022290000b2
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-04-06 22:50:59 +0800

    Lesson Example

--- team/src/main/java/com/cpt202_group10/team/Controllers/Team_Controller.java
+++ team/src/main/java/com/cpt202_group10/team/Controllers/Team_Controller.java
@@ -6,23 +6,25 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cpt202_group10.team.Models.Team;
 
 
 // Spring Anotation
 @RestController
+@RequestMapping("/team")
 public class Team_Controller
 {
     private List<Team> teams = new ArrayList<Team>();
 
-    @GetMapping("/team/list")
-    public String getList()
+    @GetMapping("/list")
+    public List<Team> getList()
     {
-        return "A list of teams";
+        return teams;
     }
 
-    @PostMapping("/team/add")
+    @PostMapping("/add")
     public void addTeam(@RequestBody Team team)
     {
         teams.add(team);
