42a27001e0e6affe2dc84e4a08e39b425727378f
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-04-09 21:45:07 +0800

    Build Pet, Groomer, User repo

--- appointment_system/src/main/java/com/cpt202/appointment_system/Repositories/PetRepo.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Repositories/PetRepo.java
@@ -2,9 +2,8 @@ package com.cpt202.appointment_system.Repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.cpt202.appointment_system.Models.Groomer;
+import com.cpt202.appointment_system.Models.Pet;
 
-
-public interface GroomerRepo extends JpaRepository<Groomer, Integer>{
+public interface PetRepo {
     
 }
