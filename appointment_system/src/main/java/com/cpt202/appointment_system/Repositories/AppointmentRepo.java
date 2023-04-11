e565f28e46c6d1da3d1d8269dca87544e3be5ab9
kdpkst <jammiechen0@gmail.com>
2023-04-11 12:38:20 +0800

    .

--- appointment_system/src/main/java/com/cpt202/appointment_system/Repositories/AppointmentRepo.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Repositories/AppointmentRepo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cpt202.appointment_system.Models.Appointment;
+import com.cpt202.appointment_system.Models.Groomer;
 import com.cpt202.appointment_system.Models.User;
 
 public interface AppointmentRepo extends JpaRepository<Appointment, Integer>{
@@ -12,7 +13,11 @@ public interface AppointmentRepo extends JpaRepository<Appointment, Integer>{
     public List<Appointment> findByUser(User user);
     
     // return the appointment list with a specific username
-    public List<Appointment> findByUsernameIs(String username);
+    public List<Appointment> findByUsernameIs(String username); 
     public Appointment findByAid(int aid);
+
+    //WJT
+    public List<Appointment> findByserviceType(String servicetype);
+    public List<Appointment> findByGroomer(Groomer groomer);
     
 }
