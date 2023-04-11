41fe917c95fbac5188689558969aae34019d93d4
JuntuoWang <Juntuo.Wang20@student.xjtlu.edu.cn>
2023-04-11 11:46:12 +0800

    Change Manger Part
    Add Search Function

--- appointment_system/src/main/java/com/cpt202/appointment_system/Services/AppointmentService.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Services/AppointmentService.java
@@ -9,6 +9,33 @@ import com.cpt202.appointment_system.Repositories.AppointmentRepo;
 public class AppointmentService {
 
     @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
     private AppointmentRepo appointmentRepo;
-    
+
+    @Autowired
+    private PetRepo petRepo;
+
+    @Autowired
+    private GroomerRepo groRepo;
+
+    //WJT Manger Part
+    //Fiter Fuction
+    public Appointment getAppointmentBy_CName(@RequestParam Appointment appointment)
+		{
+			return userRepo.findByUsernameContaining(appointment.getUser().getUsername());
+		}
+
+    public Appointment getAppointmentBy_Service(@RequestParam Appointment appointment)
+		{
+			return appointmentRepo.findByserviceType(appointment.getServiceType());
+		}
+
+    public Appointment getAppointmentBy_GrommerName(@RequestParam Appointment appointment)
+		{
+			return groRepo.findByNameContaining(appointment.getGroomer().getName());
+		}
+
+    //WJT Fiter Time
 }
