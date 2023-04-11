ecf2d5ba091c1114ecc09b06e3973ecd8b6fbecf
lostboi <100217991+humb1e1989@users.noreply.github.com>
2023-04-11 14:15:20 +0800

    Update LoginController.java

--- appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/LoginController.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/LoginController.java
@@ -2,9 +2,48 @@ package com.cpt202.appointment_system.Controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cpt202.appointment_system.Models.User;
+import com.cpt202.appointment_system.Repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/appointment-system")
 public class LoginController {
-    
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername()) || userRepository.existsByEmail(user.getEmail())) {
+            return new ResponseEntity<>("Username or email already exists.", HttpStatus.BAD_REQUEST);
+        }
+        else{
+            userRepository.save(user);
+        return new ResponseEntity<>("User registered successfully.", HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user, HttpSession session) {
+        Optional<User> dbUser = userRepository.findByUsername(user.getUsername());
+        if (dbUser.isPresent() && dbUser.get().getPassword().equals(user.getPassword())) {
+            session.setAttribute("userId", dbUser.get().getId());
+            return new ResponseEntity<>("User logged in successfully.", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("Invalid username or password.", HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logoutUser(HttpSession session) {
+        session.invalidate();
+        return new ResponseEntity<>("User logged out successfully.", HttpStatus.OK);
+    }
 }
