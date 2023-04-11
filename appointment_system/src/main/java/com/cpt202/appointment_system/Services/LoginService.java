b16d6d478f60a487328fb3f4831937feadc47de4
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-04-11 17:27:15 +0800

    // I think LoginService can be a part of Userservice

--- appointment_system/src/main/java/com/cpt202/appointment_system/Services/LoginService.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Services/LoginService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.cpt202.appointment_system.Repositories.UserRepo;
 
+// I think LoginService can be a part of Userservice
 @Service
 public class LoginService {
     
