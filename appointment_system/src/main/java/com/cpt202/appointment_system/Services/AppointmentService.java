fde8250760c27e9e4569e00ba3b4960b9f359a0f
backli1ght <1124887276@qq.com>
2023-04-11 12:50:05 +0800

    Fix some bugs.

--- appointment_system/src/main/java/com/cpt202/appointment_system/Services/AppointmentService.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Services/AppointmentService.java
@@ -1,41 +1,47 @@
 package com.cpt202.appointment_system.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cpt202.appointment_system.Models.Appointment;
+import com.cpt202.appointment_system.Models.Groomer;
+import com.cpt202.appointment_system.Models.User;
 import com.cpt202.appointment_system.Repositories.AppointmentRepo;
+import com.cpt202.appointment_system.Repositories.GroomerRepo;
+import com.cpt202.appointment_system.Repositories.PetRepo;
+import com.cpt202.appointment_system.Repositories.UserRepo;
 
 @Service
 public class AppointmentService {
 
-    @Autowired
-    private UserRepo userRepo;
+  @Autowired
+  private UserRepo userRepo;
 
-    @Autowired
-    private AppointmentRepo appointmentRepo;
+  @Autowired
+  private AppointmentRepo appointmentRepo;
 
-    @Autowired
-    private PetRepo petRepo;
+  @Autowired
+  private PetRepo petRepo;
 
-    @Autowired
-    private GroomerRepo groRepo;
+  @Autowired
+  private GroomerRepo groRepo;
 
-    //WJT Manger Part
-    //Fiter Fuction
-    public Appointment getAppointmentBy_CName(@RequestParam Appointment appointment)
-		{
-			return userRepo.findByUsernameContaining(appointment.getUser().getUsername());
-		}
+  // WJT Manger Part
+  // Fiter Fuction
+  public List<User> getAppointmentBy_CName(@RequestParam Appointment appointment) {
+    return userRepo.findByUsernameContaining(appointment.getUser().getUsername());
+  }
 
-    public Appointment getAppointmentBy_Service(@RequestParam Appointment appointment)
-		{
-			return appointmentRepo.findByserviceType(appointment.getServiceType());
-		}
+  public List<Appointment> getAppointmentBy_Service(@RequestParam Appointment appointment) {
+    return appointmentRepo.findByserviceType(appointment.getServiceType());
+  }
 
-    public Appointment getAppointmentBy_GrommerName(@RequestParam Appointment appointment)
-		{
-			return groRepo.findByNameContaining(appointment.getGroomer().getName());
-		}
+  public List<Groomer> getAppointmentBy_GrommerName(@RequestParam Appointment appointment) {
+    return groRepo.findByNameContaining(appointment.getGroomer().getName());
+  }
 
-    //WJT Fiter Time
+  // WJT Fiter Time
 }
