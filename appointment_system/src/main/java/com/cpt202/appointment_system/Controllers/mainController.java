ce6bef4d880781860baf47ebc3ce454825f6915a
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-04-15 15:45:37 +0800

    Web frontend

--- appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/mainController.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/mainController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class mainController {
+    // http://localhost:8080/
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("username", "Tomas");
-        return "home";//because it is a controller, the springboot will pull a file from the resources
+        return "home";// because it is a controller, the springboot will pull a file from the
+                      // resources
     }
 }
