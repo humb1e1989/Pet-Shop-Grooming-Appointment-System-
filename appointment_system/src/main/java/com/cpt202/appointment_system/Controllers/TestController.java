d19cbfbce226544e0ba397f7afa22cf6d3755c8e
refugee123456 <bowen.li20@student.xjtlu.edu.cn>
2023-04-15 20:17:51 +0800

    a

--- appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/TestController.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/TestController.java
@@ -1,5 +1,4 @@
 package com.cpt202.appointment_system.Controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cpt202.appointment_system.Common.Result;
 import com.cpt202.appointment_system.Models.Groomer;
 import com.cpt202.appointment_system.Services.GroomerService;
