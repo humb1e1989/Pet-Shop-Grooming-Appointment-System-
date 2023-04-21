cf30665c629551c2be368ca104e45786faf686d2
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-04-21 21:11:23 +0800

    can return to list from detail bu the opposite is still not possible

--- appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/AppointmentController.java
+++ appointment_system/src/main/java/com/cpt202/appointment_system/Controllers/AppointmentController.java
@@ -69,13 +69,13 @@ public class AppointmentController {
     }
 
     // YYY PBI NO.2 - Manager view the appointment detail
-    // @GetMapping("/manager/appointmentList/view")
-    // public String getAppointment(Model model) {// capable of convert a string into object
-    //     // model.addAttribute("appointmentList", appointmentService.listAllAppointments());
-    //     Appointment appointment = new Appointment();
-    //     model.addAttribute("appointment", appointment);
-    //     return "appointmentDetail";
-    // }
+    @GetMapping("/manager/appointmentList/view")
+    public String getAppointment(Model model) {// capable of convert a string into object
+        // model.addAttribute("appointmentList", appointmentService.listAllAppointments());
+        Appointment appointment = new Appointment(1);
+        model.addAttribute("appointment", appointment);
+        return "appointmentDetail";
+    }
     @PostMapping("/manager/appointmentList/view")
     public String viewAppointment_M(@RequestParam("appointment") Appointment appointment, Model model) {
         Appointment appointmentDetail = appointmentService.getAppointmentDetail_M(appointment);
