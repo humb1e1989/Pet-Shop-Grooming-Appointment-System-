82b1037a71435b4151355c54db6edd5a5862855a
SchwarzerYV <103877999+SchwarzerYV@users.noreply.github.com>
2023-05-01 16:06:21 +0800

    build different test folders

---
+++ src/test/java/com/cpt202/appointment_system/Integration_Test/LoginThenMakeAppointmentTest.java
@@ -0,0 +1,5 @@
+package com.cpt202.appointment_system.Integration_Test;
+
+public class LoginThenMakeAppointmentTest {
+    
+}
--- src/test/java/com/cpt202/appointment_system/WebMvcTest/makeAppointmentWebMvcTest.java
+++ src/test/java/com/cpt202/appointment_system/WebMvcTest/makeAppointmentWebMvcTest.java
@@ -1,28 +1,16 @@
-package com.cpt202.appointment_system;
+package com.cpt202.appointment_system.WebMvcTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import java.sql.Timestamp;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,14 +18,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.cpt202.appointment_system.Common.Result;
-import com.cpt202.appointment_system.Controllers.AppointmentController;
 import com.cpt202.appointment_system.Models.Appointment;
-import com.cpt202.appointment_system.Models.AppointmentForm;
 import com.cpt202.appointment_system.Models.Groomer;
 import com.cpt202.appointment_system.Models.Pet;
 import com.cpt202.appointment_system.Models.ServiceType;
 import com.cpt202.appointment_system.Models.User;
-import com.cpt202.appointment_system.Repositories.AppointmentRepo;
 import com.cpt202.appointment_system.Repositories.GroomerRepo;
 import com.cpt202.appointment_system.Repositories.PetRepo;
 import com.cpt202.appointment_system.Repositories.ServiceTypeRepo;
